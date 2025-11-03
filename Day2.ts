a7ba3d9e11b90a695873272fd5cb00d4ee54436b
Fardin Rahman <89904585+FarDin023@users.noreply.github.com>
2025-11-03 12:23:06 +0600

    Update Day2.ts

--- Day2.ts
+++ Day2.ts
@@ -76,5 +76,69 @@ console.log(safeParse(invalidJsonString));
 
 //5
 
+interface Animal {
+  name: string;
+  sound(): string;
+}
+
+class Dog implements Animal {
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  sound(): string {
+    return "Woof!";
+  }
+}
+
+const myDog = new Dog("Buddy");
+
+console.log(myDog.name);
+console.log(myDog.sound());
 
 
+//6
+
+function getlastElement<T>(arr: T[]): T | undefined {
+  if (arr.length === 0) {
+    return undefined;
+  }
+  return arr[arr.length - 1];
+}
+const emptyArray: number[] = [];
+const numberArray: number[] = [1, 2, 3, 4];
+const stringArray: string[] = ["a", "b", "c"];
+
+console.log(getlastElement(emptyArray));
+console.log(getlastElement(numberArray));
+console.log(getlastElement(stringArray));
+
+
+//7
+
+
+export function add(a: number, b: number): number {
+  return a + b;
+}
+
+export function subtract(a: number, b: number): number {
+  return a - b;
+}
+
+export function multiply(a: number, b: number): number {
+  return a * b;
+}
+
+export function divide(a: number, b: number): number {
+  if (b === 0) {
+    throw new Error("Division by zero is not allowed.");
+  }
+  return a / b;
+}
+
+console.log(add(10, 5));
+console.log(subtract(10, 5));
+console.log(multiply(10, 5));
+console.log(divide(10, 5));
